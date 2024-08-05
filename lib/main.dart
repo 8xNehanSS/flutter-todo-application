b47e51fd28d5202b8109b100c97aa129cdf21575
8xNehanSS <nehan.sudasinghe@gmail.com>
2024-08-05 10:56:00 +0530

    splash screen addition

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_todo/pages/home_page.dart';
 
-void main() {
+void main() async {
+  WidgetsBinding widgetBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetBinding);
+  await Future.delayed(Duration(seconds: 2));
+  FlutterNativeSplash.remove();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
 
   @override
   Widget build(BuildContext context) {
